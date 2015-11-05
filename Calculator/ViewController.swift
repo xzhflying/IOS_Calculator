18041c7378b97886d5c7b48826ddccd3197bc7db
Zihuan Xu <xzhflying@163.com>
2015-11-05 23:59:01 +0800

    Initial commit with some views implementation

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var userIsInputing: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if( userIsInputing )
+        {
+            display.text = display.text! + digit
+        }
+        else
+        {
+            display.text = digit
+            userIsInputing = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
