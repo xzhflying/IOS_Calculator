ddaf9f59475d9944bdb7b7038790cca6f921ba74
Zihuan Xu <xzhflying@163.com>
2015-11-07 23:40:16 +0800

    Add "(" and ")" buttons by long press action

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,15 +16,59 @@ class ViewController: UIViewController
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        
+        appendToLabel(firstButtonValue(digit))
+    }
+    
+    @IBAction func addButtonOptionalValue(sender :UILongPressGestureRecognizer) {
+        if(sender.state == .Ended)
+        {
+            appendToLabel("(")
+        }
+    }
+    
+    @IBAction func minusButtonOptionalValue(sender :UILongPressGestureRecognizer) {
+        if(sender.state == .Ended)
+        {
+            appendToLabel(")")
+        }
+    }
+    
+    func appendToLabel(str :String){
         if( userIsInputing )
         {
-            display.text = display.text! + digit
+            display.text = display.text! + str
         }
         else
         {
-            display.text = digit
+            display.text = str
             userIsInputing = true
+        }
+    }
+    
+    func firstButtonValue(str :String) -> String{
+        if (str.containsString("/"))
+        {
+            let index = str.startIndex.advancedBy(1)
+            return str.substringToIndex(index)
+        }
+       else
+        {
+            return str
+        }
+    }
+    
+    // Unused for now
+    func secondButtonValue(str :String) -> String?{
+        if (str.containsString("/"))
+        {
+            let startIndex = str.startIndex.advancedBy(1)
+            let endIndex = str.startIndex.advancedBy(2)
+            let range = Range(start: startIndex, end: endIndex)
+            return str.substringWithRange(range)
+        }
+        else
+        {
+            return nil
         }
     }
 }
